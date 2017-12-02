ec3d610acd9bf00d6f52259d33ee156e41b6bc6b
Matt Parsons <parsonsmatt@gmail.com>
2017-12-01 19:05:28 -0700

    tutorial step two

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import Control.Concurrent (threadDelay)
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Control.Monad (forever)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
 
@@ -9,8 +11,21 @@ main = do
     Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
     node <- newLocalNode t initRemoteTable
     _ <- runProcess node $ do
+        echoPid <- spawnLocal $ forever $
+            receiveWait
+                [ match $ \(sender, msg) -> send sender (msg :: String)
+                , match $ \msg -> say $ "handling " ++ msg
+                ]
+
+        say "sending messages"
+        send echoPid "hello"
         self <- getSelfPid
-        send self "hello"
-        hello <- expect :: Process String
-        liftIO $ putStrLn hello
+        send echoPid (self, "hello world")
+
+        m <- expectTimeout 10000000
+        case m of 
+            Nothing -> die "no words"
+            Just s -> say $ "got " ++ s ++ " back!"
+
+        liftIO $ threadDelay 20000000
     return ()
